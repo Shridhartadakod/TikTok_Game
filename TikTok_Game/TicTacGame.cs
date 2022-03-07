b2acbe373dc6c5f54d6841c9218e56944177c37d
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-07 11:07:50 +0530

    choose input between X and O

--- TikTok_Game/TicTacGame.cs
+++ TikTok_Game/TicTacGame.cs
@@ -8,14 +8,35 @@ namespace TikTok_Game
 {
     public class TicTacGame
     {
+        public static char player, computer;
         public static void DrawBoard()
         {
             char[] board = new char[10];
-            for (int i = 0; board.Length > 0; i++)
+            for (int i = 1; board.Length < 10; i++)
             {
                 Console.WriteLine(board[i] = ' ');
             }
             Console.ReadLine();
+        }
+
+        public static void PlayerInput()
+        {
+            player = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch (player)
+            {
+                case 'X':
+                    computer O;
+                    Console.WriteLine("Player Input:" + player);
+                    Console.WriteLine("Computer char is :" + computer);
+                    break;
+                case 'O':
+                    computer X;
+                    Console.WriteLine("Player Input:" + player);
+                    Console.WriteLine("Computer char is :" + computer);
+                    break;
+                default:
+                    Console.WriteLine("invalid");
+            }
         }
     }
 }
